248b87977651376c9d33b2997099304e63c413af
Rafel Palliser Sans <rafel1995@gmail.com>
2021-07-06 10:12:16 +0200

    Update used objects

--- gym_simple_minigrid/minigrid.py
+++ gym_simple_minigrid/minigrid.py
@@ -37,26 +37,8 @@ COLOR_TO_IDX = {k: i for i, k in enumerate(COLORS)}
 
 # Map of object type to integers
 OBJECT_TO_IDX = {
-    'unseen'        : 0,
-    'empty'         : 1,
-    'wall'          : 2,
-    'floor'         : 3,
-    'door'          : 4,
-    'key'           : 5,
-    'ball'          : 6,
-    'box'           : 7,
-    'goal'          : 8,
-    'lava'          : 9,
-    'agent'         : 10,
-}
-
-IDX_TO_OBJECT = dict(zip(OBJECT_TO_IDX.values(), OBJECT_TO_IDX.keys()))
-
-# Map of state names to integers
-STATE_TO_IDX = {
-    'open'  : 0,
-    'closed': 1,
-    'locked': 2,
+    'wall': 0,
+    'goal': 1,
 }
 
 # Map of agent direction indices to vectors
