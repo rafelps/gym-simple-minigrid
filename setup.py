7e1f036c391abc8e9e0324ae6bc11378806aad37
Rafel Palliser Sans <rafel1995@gmail.com>
2021-07-06 11:59:15 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,11 @@
 from setuptools import setup
 
 setup(
-    name='gym_minigrid',
-    version='1.0.2',
+    name='gym_simple_minigrid',
+    version='2.0.0',
     keywords='memory, environment, agent, rl, openaigym, openai-gym, gym',
-    url='https://github.com/maximecb/gym-minigrid',
-    description='Minimalistic gridworld package for OpenAI Gym',
-    packages=['gym_minigrid', 'gym_minigrid.envs'],
-    install_requires=[
-        'gym>=0.9.6',
-        'numpy>=1.15.0'
-    ]
+    url='https://github.com/rafelps/gym-simple-minigrid',
+    description='Simple gridworld package for OpenAI Gym with multi-goal support',
+    packages=['gym_simple_minigrid', 'gym_simple_minigrid.envs'],
+    install_requires=['gym', 'numpy', 'matplotlib']
 )
